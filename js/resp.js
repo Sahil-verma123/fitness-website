5115a49529cc1536dda32dbc94ea68f09e91d304
sahil verma <sahil.verma15231@gmail.com>
2021-09-01 00:17:24 +0530

    initial commit

---
+++ js/resp.js
@@ -0,0 +1,13 @@
+// burger = document.querySelector('.burger')
+// navbar = document.querySelector('.navbar')
+// rightnav = document.querySelector('.rightnav')
+// leftnav = document.querySelector('.leftnav')
+
+// burger.addEventListner('click',()=>{
+//     rightnav.classList.toggle('v-class');
+//     leftnav.classList.toggle('v-class');
+//     navbar.classList.toggle('h-nav');
+
+// })
+
+
